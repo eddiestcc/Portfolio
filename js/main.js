252ae89bdb49ac30219ef1337bbfad81bfec2821
Your Namceclear <you@example.com>
2022-09-21 22:17:38 -0400

    Made some UX/UI changes to interface.

--- js/main.js
+++ js/main.js
@@ -61,4 +61,14 @@ Menu
 
 })();
 
+function update(e){
+	let x = e.clientX || e.touches[0].clientX
+	let y = e.clientY || e.touches[0].clientY
+  
+	document.documentElement.style.setProperty('--cursorX', x + 'px')
+	document.documentElement.style.setProperty('--cursorY', y + 'px')
+  }
+  
+  document.addEventListener('mousemove',update)
+  document.addEventListener('touchmove',update)
 
